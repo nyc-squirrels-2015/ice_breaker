6a4627297c1e8f650861f6f741a433125e15ebb9
garygz <garyginzburg@hotmail.com>
2015-02-06 14:25:00 -0500

    added models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :quotes
+  has_many :subcategories, through: :quotes
   # Remember to create a migration!
 end
--- db/migrate/20150206122849_create_quotes.rb
+++ db/migrate/20150206122849_create_quotes.rb
@@ -1,4 +1,10 @@
 class CreateQuotes < ActiveRecord::Migration
   def change
+    create_table  :quotes do |t|
+      t.text         :text
+      t.references   :user
+      t.references   :subcategory
+      t.binary       :image, limit: 1.megabytes
+    end
   end
 end
