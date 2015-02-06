2e6a020f72ca597d8c04294abb61e7f543c5901d
garygz <garyginzburg@hotmail.com>
2015-02-06 12:29:31 -0500

    commit migrations

--- db/migrate/20141020120711_create_users.rb
+++ db/migrate/20141020120711_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password_digest
+      t.string :name, null:false
+      t.string :email, null:false
+      t.binary :image, limit: 1.megabytes
+      t.string :password_digest, null:false
     end
   end
 end
