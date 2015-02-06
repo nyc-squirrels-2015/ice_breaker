53c87717d9d37a76079da265f5ad643d40371954
Dongchul Lee & Rosa Pergams <pair+i-love-ruby.rosapergams@devbootcamp.com>
2015-02-06 17:03:42 -0500

    added authentication routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,27 @@
-get '/' do
-  erb :welcome
+get '/signup' do
+  erb :'/auth/signup'
+end
+
+post '/signup' do
+  a_user = User.create(params[:user])
+  session[:user] = a_user.id
+  redirect '/'
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
 
 get '/login' do
-  
+  erb :'auth/login'
 end
 
-get '/signup' do
-  
+post '/login' do
+  a_user = User.find_by(:email => params[:user][:email]).try(:authenticate, params[:user][:password])
+  if a_user
+    session[:user] = a_user.id
+    p "WINNING"
+  end
+  redirect '/'
 end
